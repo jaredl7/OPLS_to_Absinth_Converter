483a66631e9c18ff0b0d1e5b9c457eeb7bc6fe41
Jared Lalmansingh <jared.lalmansingh@wustl.edu>
2020-04-16 21:00:29 -0500

    Added the ablity to parse OPLS bonds, angles, dihedrals, nonbonded-atoms, charges, contacts, and interacts.

--- parameterize.py
+++ parameterize.py
@@ -3,7 +3,8 @@ This file is meant to contain code related to validating and updating the CHARMM
 parameter using using the OPLS parameter and topology files.
 """
 from parsers import OPLS_Topology_Parser, OPLS_Parameters_Parser, Absinth_Parameters_Parser
-from common_objects import BioType, AtomType
+from common_objects import BioType, AtomType, ChargeType, InteractType, ContactType
+from common_objects import BondLength, BondAngle, DihedralAngle
 from collections import OrderedDict
 from itertools import chain
 from pprint import pprint
@@ -14,6 +15,7 @@ import re
 
 
 ABSINTH_BIOTYPES_CUTOFF = 1208
+SCALING_CONSTANT = 2.0**(1.0/6.0)
 
 
 def parse_files(absinth_parameter_file, opls_topology_file, opls_parameter_file):
@@ -247,16 +249,91 @@ def add_OPLS_LJ_types(residue, opls_topology, current_atom_type):
         new_atom_type = AtomType(current_atom_type, atom_name, description, atomic_number, atom_mass, valence)
         new_atom_types.append(new_atom_type)
         current_atom_type += 1
-        # print(new_atom_type)
+        #print(new_atom_type)
+        #print(atom_name, atom_mass_class)
     return new_atom_types, current_atom_type
-        
+
+
+def add_OPLS_contact_and_interact_types(residue, opls_parameters):
+    # TODO: REVISE
+    pass
+
+
+def add_OPLS_bonds(opls_parameters, starting_bond_number, potential_type=1):
+    # This one is a little tricky. For the bondlengths (i.e. "bond" in Absinth parlance),
+    # the two atoms are not required when defining the bond (that's done in bonded_type_bond).
+    # So, all we have to do here is create a new "bond" from the last bond number.
+    bonds = list()
+    for opls_bond in opls_parameters.bonds:
+        bond = BondLength(starting_bond_number, potential_type, opls_bond.spring_constant, opls_bond.reference_bond_length, 0)  # there is no `c`
+        starting_bond_number += 1
+        bonds.append(bond)
+    return bonds
+
+
+def add_OPLS_angles(opls_parameters, starting_angle_number, potential_type=1):
+    angles = list()
+    for opls_angle in opls_parameters.angles:
+        angle = BondAngle(starting_angle_number, potential_type, opls_angle.theta_force_constant, opls_angle.reference_angle, 0, 0)  # No Urey-Bradley terms
+        starting_angle_number += 1
+        angles.append(angle)
+    return angles
+
+
+def add_OPLS_dihedrals(opls_parameters, starting_dihedral_number, potential_type=1):
+    dihedrals = list()
+    for opls_dihedral in opls_parameters.dihedrals:
+        dihedral = DihedralAngle(starting_dihedral_number, potential_type, 0, 0, 0, 0, 0, 0, 0)  # No Urey-Bradley terms
+        starting_dihedral_number += 1
+        dihedrals.append(dihedral)
+    return dihedrals
+
+
+def add_OPLS_biotypes(residue, opls_topology, current_biotype):
+    new_biotypes = list()
+    for atom_name in residue.atoms:
+        possible_LJ_type = atom_name
+        trailing_description = ""
+
+        # this is for the case where the first letter is a number
+        if possible_LJ_type[0].isnumeric():
+            possible_LJ_type = atom_name[1:]
+
+        # now define the attributes for the `LJ_type`
+        number = copy.copy(current_atom_type)
+        element = copy.copy(possible_LJ_type)
+        description = "\"OPLS %s (%s)\"" % (residue.name, atom_name)
+
+        current_biotype += 1
+    return new_biotypes, current_biotype
 
 
 def add_OPLS_patched_residues(absinth_parameters_file, opls_topology_file, opls_parameters_file, patched_residue_names_file):
     # This is the directive: take the patched OPLS residues, and add them to the biotypes.
     absinth_parser, opls_topology, opls_parameters = parse_files(absinth_parameters_file, opls_topology_file, opls_parameters_file)
     patched_names = load_yaml_file(patched_residue_names_file)['friendly-names']
+
+    current_bond_number = absinth_parser.bonds[-1].identifier + 1
+    current_angle_number = absinth_parser.angles[-1].identifier + 1
+    current_dihedral_number = absinth_parser.torsions[-1].identifier + 1
     current_atom_type = absinth_parser.atoms[-1].number + 1
+    current_contact_type = absinth_parser.contacts[-1].i + 1
+    current_interact_type = absinth_parser.interacts[-1].i + 1
+    current_charge_type = absinth_parser.charges[-1].number + 1
+    current_biotype = absinth_parser.biotypes[-1].number + 1
+
+    new_bonds = add_OPLS_bonds(opls_parameters, current_bond_number)
+    new_angles = add_OPLS_angles(opls_parameters, current_angle_number)
+    new_dihedrals = add_OPLS_dihedrals(opls_parameters, current_dihedral_number)
+
+    nonbonded_atoms = [nb.atom for nb in opls_parameters.nonbonded]
+    new_contacts_by_mass_class = OrderedDict()
+    new_contacts_by_number = OrderedDict()
+    new_interacts_by_mass_class = OrderedDict()
+    new_interacts_by_number = OrderedDict()
+    new_contacts = list()
+    new_interacts = list()
+    new_charges = list()
 
     # OK - so as I understand it, this has to work in the following manner:
     # To create a biotype, we need to define:
@@ -274,4 +351,42 @@ def add_OPLS_patched_residues(absinth_parameters_file, opls_topology_file, opls_
         new_atom_types, current_atom_type = add_OPLS_LJ_types(residue, opls_topology, current_atom_type)
         all_new_atom_types += new_atom_types  # update the atom_types / LJ_types
 
+        # need to add the contact and interact parameters for the atom / LJ_types
+        #add_OPLS_contact_and_interact_types(residue, opls_parameters)
+        for atom_name in residue.atoms:
+            atom = residue.atoms[atom_name]
+            index = nonbonded_atoms.index(atom.mass_class)
 
+            if atom.mass_class not in new_contacts_by_mass_class:
+                new_contacts_by_mass_class[atom.mass_class] = (atom_name, copy.copy(current_contact_type))
+                new_contacts_by_number[current_contact_type] = (atom_name, atom.mass_class)
+                current_contact_type += 1
+
+                match = new_contacts_by_mass_class[atom.mass_class]
+                i = copy.copy(match[-1])
+                j = copy.copy(match[-1])
+                sigma = 2 * opls_parameters.nonbonded[index].Rmin2 / SCALING_CONSTANT  # Rmin2 means (Rmin / 2)
+                contact_type = ContactType(i, j, sigma)
+                new_contacts.append(contact_type)
+
+            if atom.mass_class not in new_interacts_by_mass_class:
+                new_interacts_by_mass_class[atom.mass_class] = (atom_name, copy.copy(current_interact_type))
+                new_interacts_by_number[current_interact_type] = (atom_name, atom.mass_class)
+                current_interact_type += 1
+
+                match = new_interacts_by_mass_class[atom.mass_class]
+                i = copy.copy(match[-1])
+                j = copy.copy(match[-1])
+                epsilon = opls_parameters.nonbonded[index].epsilon
+                interact_type = InteractType(i, j, epsilon)
+                new_interacts.append(interact_type)
+
+            # next, add the charge types
+            description = "\"OPLS %s (%s)\"" % (residue.name, atom_name)
+            charge_type = ChargeType(current_charge_type, description, atom.charge)
+            current_charge_type += 1
+            new_charges.append(charge_type)
+
+        # then the bonded types (this is a huge table dump)
+
+        # finally, the biotypes
